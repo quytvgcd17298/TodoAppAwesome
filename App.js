ff3075c8011881cee0dafa884222cc48ebba1407
Your Name <quytvgcd17298@fpt.edu.vn>
2021-09-12 21:50:25 +0700

    Complete Add task function

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useState } from "react";
 import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, Button } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [ text, setText] = useState("");
+  const [ tasks, setTasks] = useState([]);
+  const handleButton = () => {
+    setTasks([...tasks, text]);
+    setText("");
+  };
   return (
     <View style={styles.container}>
       <View style = {styles.item}>
-      <Text style = {{ fontSize: 24}}>Today Tasks</Text>
+      <Text style = {{ fontSize: 24}}>Today Tasks !</Text>
       </View>
       <View>
-        <Task text = "Task 1"></Task>
-        <Task text = "Task 2"></Task>
-        <Task text = "Task 3"></Task>
-        <Task text = "Task 4"></Task>
+        {tasks.map((task,index) => {
+        return (
+          <TouchableOpacity
+          key = {index}>
+          <Task text = {task}></Task>
+          </TouchableOpacity>
+        )})}
       </View>
+
       <KeyboardAvoidingView
       style = {styles.addTaskWrapper}>
         <TextInput
+        value = {text}
+        onChangeText = {(value) => setText(value)}
         style = {styles.input}
         placeholder = 'Add task ...'
         ></TextInput>
         <Button
+        onPress = {handleButton}
         title = 'Add'
         color = 'blue'
         ></Button>
